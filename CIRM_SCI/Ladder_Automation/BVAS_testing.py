2620712f769d0e1d35bdc588153c841a0b9a8b1d
hstacey <107888618+hstacey@users.noreply.github.com>
2022-11-16 14:32:33 -0800

    Create BVAS_testing.py

---
+++ CIRM_SCI/Ladder_Automation/BVAS_testing.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 16 14:30:08 2022
+
+@author: hollandstacey
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import ndimage
+import mat73
+import pywt
+import pdb #this is a pause button tool
+from Backend import *
+from skimage import filters
+import os, cv2
+from copy import copy
+from matplotlib import cm
+from skimage import measure
+
+#this is where ladder automation code will go
+
